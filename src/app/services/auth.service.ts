5436fcd2dc60900ba057fc3987859510505bbfb0
jim27xim <jim27xim@gmail.com>
2022-11-14 17:29:08 -0300

    Detalle Ruta autenticacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  url = '/api-usuario';
+  url = '/api-usuario/singin';
   user$: any;
   constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {}
 
   singin(user: any) {
-    return this.http.post('RutasUsuario', user);
+    return this.http.post(this.url, user);
   }
 
   isAuth(): boolean {
