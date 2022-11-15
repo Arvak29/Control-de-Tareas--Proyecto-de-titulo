9c8c172fee3f9c79a533e61c23b414d7896087f9
jim27xim <jim27xim@gmail.com>
2022-11-15 13:52:34 -0300

    Nav active

--- src/app/home/flujo-de-tareas/crear-flujo-de-tareas/crear-flujo-de-tareas.component.ts
+++ src/app/home/flujo-de-tareas/crear-flujo-de-tareas/crear-flujo-de-tareas.component.ts
@@ -23,7 +23,7 @@ export class CrearFlujoDeTareasComponent implements OnInit {
       fecha_inicio_ft: ['', Validators.required],
       fecha_entrega_ft: ['', Validators.required],
       porcentaje_avance_ft: ['', Validators.required],
-      estado_ft: ['', Validators.required]
+      estado_ft: ['', Validators.required],
     });
   }
 
@@ -32,10 +32,15 @@ export class CrearFlujoDeTareasComponent implements OnInit {
   crear_flujo_de_tareas() {
     const Flujo: AgregarFlujo = {
       nombre_ft: this.Crear_Flujo_de_tareas_Grupo.get('nombre_ft')?.value,
-      descripcion_ft: this.Crear_Flujo_de_tareas_Grupo.get('descripcion_ft')?.value,
-      fecha_inicio_ft: this.Crear_Flujo_de_tareas_Grupo.get('fecha_inicio_ft')?.value,
-      fecha_entrega_ft: this.Crear_Flujo_de_tareas_Grupo.get('fecha_entrega_ft')?.value,
-      porcentaje_avance_ft: this.Crear_Flujo_de_tareas_Grupo.get('porcentaje_avance_ft')?.value,
+      descripcion_ft:
+        this.Crear_Flujo_de_tareas_Grupo.get('descripcion_ft')?.value,
+      fecha_inicio_ft:
+        this.Crear_Flujo_de_tareas_Grupo.get('fecha_inicio_ft')?.value,
+      fecha_entrega_ft:
+        this.Crear_Flujo_de_tareas_Grupo.get('fecha_entrega_ft')?.value,
+      porcentaje_avance_ft: this.Crear_Flujo_de_tareas_Grupo.get(
+        'porcentaje_avance_ft'
+      )?.value,
       estado_ft: this.Crear_Flujo_de_tareas_Grupo.get('estado_ft')?.value,
     };
     this.FlujoService.addFlujo(Flujo).subscribe();
